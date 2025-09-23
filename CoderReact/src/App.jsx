0d03718fdcbe0484e9cb0f322e1d8d742a0e982e
Bautista Sosa <bsprogramacion@gmail.com>
2025-09-23 15:11:16 -0300

    realziando primer entrega

---
+++ CoderReact/src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+import { Nav } from './componentes/nav';
+
+
+
+function App() {
+  return <Nav />;
+}
+
+export default App;
---
+++ CoderReact/src/componentes/Nav.jsx
@@ -0,0 +1,18 @@
+
+
+export const Nav = () => (
+  <>
+  <img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" alt="" />
+    <ul>
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">Catalog</a>
+      </li>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+    </ul>
+  </>
+);
